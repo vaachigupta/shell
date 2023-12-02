dc34343cd3b0edd49f28b91deb7fbb41fb17ad01
vaachigupta <149156873+vaachigupta@users.noreply.github.com>
2023-12-02 16:27:22 +0530

    Add files via upload

---
+++ lab4/arithmetic.sh
@@ -0,0 +1,6 @@
+read -p "Enter two numbers" a b
+echo "Addition of the two numbers is" $((a+b))
+echo "Subtraction of the two numbers is" $((a-b))
+echo "Division of the two numbers is" $((a/b))
+echo "Multiplication of the two numbers is" $((a*b))
+

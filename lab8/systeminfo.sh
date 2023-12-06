d73ed73659d67818cc1ccc295c159e8308156cd8
vaachigupta <149156873+vaachigupta@users.noreply.github.com>
2023-12-06 14:27:03 +0530

    Add files via upload

---
+++ lab8/systeminfo.sh
@@ -0,0 +1,9 @@
+now=$(date)
+echo "The curent date and time is:"
+echo "$now"
+who=$(users)
+echo "The current logged in users are:"
+echo "$who"
+upto=$(uptime)
+echo "Current system uptime:"
+echo "$upto"
